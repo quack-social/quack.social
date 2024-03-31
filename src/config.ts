cc1c242a269c686ef7d346db02b7941718e083ab
Ahwx <ahwx@ahwx.org>
2024-03-31 18:26:42 +0200

    feat: adds site-wide configuration

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+
+export const SITE_TITLE = "Quack"
+export const SITE_DESCRIPTION = "Quack is a friendly social place on the internet"
+export const HOMEPAGE_URL = "https://quack.social"
